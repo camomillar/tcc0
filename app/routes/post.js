491a772fa868793076280822a26076fe56961eac
Danrley <danrleyt@gmail.com>
2018-12-03 17:16:01 -0300

    debug

--- app/routes/post.js
+++ app/routes/post.js
@@ -4,7 +4,7 @@ let auth = require('../controllers/auth')
 module.exports= function(app){
     app.get('/api/posts', post.getPosts)
     app.get('/api/posts/:id', post.getPostById)
-    app.get('/api/posts/like/:id', post.likePost)
+    app.get('/api/posts/like/:_id', post.likePost)
     app.post('/api/posts', post.inserirPost)
     app.put('/api/posts/:id', post.updatePostById)
     app.delete('/api/posts/:id', post.removePostById)
